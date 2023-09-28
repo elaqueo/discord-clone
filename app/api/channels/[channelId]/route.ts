416571de0ed50b83271bdd682deeac85b75d63b3
Pablo Daniel Britos <elaqueo@gmail.com>
2023-09-28 11:01:12 -0300

    edit channels

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -1,6 +1,5 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { profile } from "console";
 import { NextResponse } from "next/server";
 
 export async function DELETE(
@@ -45,6 +44,64 @@ export async function DELETE(
     return NextResponse.json(server);
   } catch (error) {
     console.error("[CHANNEL_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { channelId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    const { name, type } = await req.json();
+
+    const { searchParams } = new URL(req.url);
+    const serverId = searchParams.get("serverId");
+    if (!serverId)
+      return new NextResponse("Server Id Required", { status: 400 });
+    if (!params.channelId)
+      return new NextResponse("Channel Id Required", { status: 400 });
+    if (name === "general")
+      return new NextResponse("Name cannot be general", {
+        status: 400,
+      });
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: ["ADMIN", "MODERATOR"],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: params.channelId,
+              NOT: {
+                name: "general",
+              },
+            },
+            data: {
+              name,
+              type,
+            },
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.error("[CHANNEL_ID_PATCH]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
